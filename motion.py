893f32eff32b010573d1713e697d197e324d3ddd
AllenF88 <38099069+AllenF88@users.noreply.github.com>
2018-05-03 22:36:12 +0200

    Update motion.py

--- motion.py
+++ motion.py
@@ -11,23 +11,26 @@ GPIO.setmode(GPIO.BCM)
 PIR_PIN=7
 GPIO.setup(PIR_PIN,GPIO.IN)
 chat_id=330392544
+
+home_dir='/home/pi/Jarvis/JarvisHomeMonitoring'
+
 botKey = ""
 
-with open('/home/pi/JarvisMobile/cfg/bot.cfg','r') as bot_cfg_file:
-	for line in bot_cfg_file:
-		botKey = line.strip()
-    print (botKey)
+with open(home_dir + '/cfg/bot.cfg','r') as bot_cfg_file:
+        for line in bot_cfg_file:
+                botKey = line.strip()
 
 def sendSnap():
         # Connect to our bot
         botMotion = telegram.Bot(token=botKey)
         #Get the photo
-        camera=picamera.PiCamera()
-        timeStamp=time.strftime("%Y%m%d_%H%M%S")
-        filename="/home/pi/JarvisMobile/captures/"+timeStamp+"_Capture.jpg"
+        camera = picamera.PiCamera()
+        timeStamp = time.strftime("%Y%m%d_%H%M%S")
+        filename = home_dir + "/captures/"+timeStamp+"_Capture.jpg"
         camera.capture(filename)
         print("Photo taken")
         camera.close()
+
         # Sends a message to the chat
         botMotion.sendPhoto(chat_id=chat_id, photo=open(filename, 'rb'))
         print("Photo sent")
@@ -40,7 +43,7 @@ try:
     while True:
             if GPIO.input(PIR_PIN):
                 print "Motion Detected!"
-		sendSnap()
+                sendSnap()
             time.sleep(1)
             
 except KeyboardInterrupt:
