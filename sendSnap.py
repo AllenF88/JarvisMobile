e2e7f63639aac4c0261f543da76a264fd4abac1b
AllenF88 <38099069+AllenF88@users.noreply.github.com>
2018-05-03 22:32:48 +0200

    Update sendSnap.py

--- sendSnap.py
+++ sendSnap.py
@@ -6,23 +6,27 @@ import datetime
 import telegram
 import picamera
 
+home_dir='/home/pi/Jarvis/JarvisHomeMonitoring'
+
 botKey = ""
 
-with open('/home/pi/JarvisMobile/cfg/bot.cfg','r') as bot_cfg_file:
-	for line in bot_cfg_file:
-		botKey = line.strip()
-    print (botKey)
+with open(home_dir + '/cfg/bot.cfg','r') as bot_cfg_file:
+        for line in bot_cfg_file:
+                botKey = line.strip()
 
 chat_id=int(str(sys.argv[1]))
 print("script called, with " + str(chat_id))
+
 # Connect to our bot
 bot = telegram.Bot(token=botKey)
+
 #Get the photo
-camera=picamera.PiCamera()
-timeStamp=time.strftime("%Y%m%d_%H%M%S")
-filename="/home/pi/JarvisMobile/captures/"+timeStamp+"_Capture.jpg"
+camera = picamera.PiCamera()
+timeStamp = time.strftime("%Y%m%d_%H%M%S")
+filename = home_dir + "/captures/"+timeStamp+"_Capture.jpg"
 camera.capture(filename)
 print("Photo taken")
 camera.close()
+
 bot.sendPhoto(chat_id=chat_id, photo=open(filename, 'rb'))
 print("Photo sent")
